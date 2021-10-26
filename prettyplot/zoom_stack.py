5b5856fd9da099404ebbcd0fe1a990a003bef62e
designingSparks <surfer10@gmail.com>
2021-10-26 16:58:44 +0200

    Zoom stack now working.

--- prettyplot/zoom_stack.py
+++ prettyplot/zoom_stack.py
@@ -11,9 +11,6 @@ Usage:
 To initialize, pass the plots that the zoomstack should manage to the contructer.
 Plot the data on the plot
 Call the function initZoomStack.
-
-TODO:
-Complete documentation
 '''
 
 
@@ -36,7 +33,6 @@ class ZoomStack(QObject):
         for plot_widget in plot_list:
             plot_widget.plotItem.vb.sigZoom.connect(self.addToZoomStack)
             self.vbox_list.append(plot_widget.plotItem.vb) #store viewbox of the plot item for recalling zoom
-        # self.initZoomStack()
 
 
     def initZoomStack(self):
@@ -46,7 +42,6 @@ class ZoomStack(QObject):
         will be stored in pozition 0 on the stack.
         '''
         zoom_state = list()
-
         for i, viewbox in enumerate(self.vbox_list):
             vr = viewbox.viewRange() #note the viewrange does not match the coordinates
             bottom = vr[1][0]
@@ -59,31 +54,46 @@ class ZoomStack(QObject):
             logger.debug(rect)
             zoom_state.append(i)
             zoom_state.append(rect)
-        
+        logger.debug('Initial zoom state: {}'.format(zoom_state))
         self.stack.append(zoom_state)
-        # logger.debug('Stored initial zoom state: {}'.format(zoom_state))
 
 
     def zoom_home(self):
         logger.debug('Restoring home view')
-        self.recall_view(self.stack[0])
+        last_ax = self.stack[self.zoom_pos]
+        home_state = self.stack[0]
+
+        if last_ax != home_state:
+            logger.debug('Adding home view to stack')
+            self.recall_view(home_state)
+            self.addToZoomStack(home_state)
         
 
     def recall_view(self, zoom_state):
         '''
-        zoom_state is an entry in the self.stack
+        Recalls a specific view or views in the zoom_state list. Called from zoom_back() or zoom_forward() 
+        Parameters:
+        zoom_state - a list of views to be restored
+        The format is [i0, rect1, i1, rect2...], where i is the index of the viewbox in self.vbox_list
         '''
-        for idx, rect in zip(*[iter(zoom_state)]*2): #first item in stack
+        for idx, rect in zip(*[iter(zoom_state)]*2): #iterate two items at a time
             # print(idx, rect)
             logger.debug('Recalling view')
             viewbox = self.vbox_list[idx]
             viewbox.showAxRect(rect, padding=0) #recall view
 
 
-    def addToZoomStack(self, viewbox, rect):
-        
-        idx = self.vbox_list.index(viewbox) 
-        zoom_state = [idx, rect]
+    def addToZoomStack(self, *args): #change to args
+        '''
+        Called from ViewBox.py at the completion of a zoom operation or from zoom_home().
+        '''
+        zoom_state = None
+        if len(args) == 2:
+            viewbox, rect = args
+            idx = self.vbox_list.index(viewbox) 
+            zoom_state = [idx, rect]
+        elif len(args) == 1:
+            zoom_state = args[0]
         self.stack.append(zoom_state) #store the view
         self.zoom_pos += 1
 
@@ -94,8 +104,6 @@ class ZoomStack(QObject):
             logger.debug('Pruning zoom stack. n={}'.format(n))
             self.stack = self.stack[:self.zoom_pos+1]
 
-
-        logger.debug('Adding to zoom stack. Viewbox: {}'.format(idx))
         logger.debug('Number of items in stack: {}'.format(len(self.stack)))
         logger.debug('zoom_pos: {}'.format(self.zoom_pos))
         
