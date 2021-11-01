753a51f3552244110467a830777786c6f797eacb
designingSparks <surfer10@gmail.com>
2021-11-01 12:08:31 +0100

    Cleanup zoom_stack.py

--- kiwiplot/zoom_stack.py
+++ kiwiplot/zoom_stack.py
@@ -46,27 +46,12 @@ class ZoomStack(QObject):
         The data must be plotted in the viewbox first otherwise the default unit rectangle
         will be stored in pozition 0 on the stack.
         '''
-        # zoom_state = list()
-        # for i, viewbox in enumerate(self.vbox_list):
-        #     vr = viewbox.viewRange() #note the viewrange does not match the coordinates
-        #     bottom = vr[1][0]
-        #     top = vr[1][1]
-        #     left = vr[0][0]
-        #     right = vr[0][1]
-        #     _p1 = QPointF(left, top)
-        #     _p2 = QPointF(right, bottom)
-        #     rect = QRectF(_p1, _p2)
-        #     logger.debug(rect)
-        #     zoom_state.append(i)
-        #     zoom_state.append(rect)
-        # logger.debug('Initial zoom state: {}'.format(zoom_state))
-        # self.stack.append(zoom_state)
         zoom_state = list()
         for i, viewbox in enumerate(self.vbox_list):
             vr = viewbox.viewRange() #note the viewrange does not match the coordinates
             zoom_state.append(i)
             zoom_state.append(vr)
-            logger.debug('{}, {}'.format(i, vr))
+        logger.debug('Initial zoom state: {}'.format(zoom_state))
         self.stack.append(zoom_state) #store the view
 
 
@@ -95,13 +80,6 @@ class ZoomStack(QObject):
             self.addToZoomStack(home_state)
         
 
-    def recall_view2(self, zoom_state):
-        for idx, rect in zip(*[iter(zoom_state)]*2): #iterate two items at a time
-            print(idx, rect)
-            viewbox = self.vbox_list[idx]
-            viewbox.setRange(xRange= rect[0], yRange=rect[1], padding=0)
-
-
     def recall_view(self, zoom_state):
         '''
         Recalls a specific view or views in the zoom_state list. Called from zoom_back() or zoom_forward() 
@@ -110,37 +88,21 @@ class ZoomStack(QObject):
         The format is [i0, rect1, i1, rect2...], where i is the index of the viewbox in self.vbox_list
         '''
         for idx, rect in zip(*[iter(zoom_state)]*2): #iterate two items at a time
-            # print(idx, rect)
-            logger.debug('Recalling view. idx: {}'.format(idx))
+            print(idx, rect)
             viewbox = self.vbox_list[idx]
-            viewbox.showAxRect(rect, padding=0) #recall view
+            viewbox.setRange(xRange= rect[0], yRange=rect[1], padding=0)
 
 
     def updateZoomStack(self):
+        '''
+        Called from ViewBox.py at the completion of a zoom operation.
+        '''
         zoom_state = list()
         for i, viewbox in enumerate(self.vbox_list):
-            vr = viewbox.viewRange() #note the viewrange does not match the coordinates
+            vr = viewbox.viewRange() #get viewrange of axis
             zoom_state.append(i)
             zoom_state.append(vr)
             logger.debug('{}, {}'.format(i, vr))
-        self.stack.append(zoom_state) #store the view
-        self.zoom_pos += 1
-        self.sigEnableBack.emit(self.zoom_pos > 0)
-        self.sigEnableForward.emit(self.zoom_pos < (len(self.stack) - 1))
-        logger.debug('Added to zoom stack: {}'.format(zoom_state))
-
-
-    def addToZoomStack(self, *args): #change to args
-        '''
-        Called from ViewBox.py at the completion of a zoom operation or from zoom_home().
-        '''
-        zoom_state = None
-        if len(args) == 2:
-            viewbox, rect = args
-            idx = self.vbox_list.index(viewbox) 
-            zoom_state = [idx, rect]
-        elif len(args) == 1:
-            zoom_state = args[0]
         self.stack.append(zoom_state) #store the view
         self.zoom_pos += 1
 
@@ -151,12 +113,13 @@ class ZoomStack(QObject):
             logger.debug('Pruning zoom stack. n={}'.format(n))
             self.stack = self.stack[:self.zoom_pos+1]
 
-        logger.debug('Number of items in stack: {}'.format(len(self.stack)))
-        logger.debug('zoom_pos: {}'.format(self.zoom_pos))
         self.sigEnableBack.emit(self.zoom_pos > 0)
         self.sigEnableForward.emit(self.zoom_pos < (len(self.stack) - 1))
 
+        logger.debug('Number of items in stack: {}'.format(len(self.stack)))
+        logger.debug('zoom_pos: {}'.format(self.zoom_pos))
         logger.debug('Added to zoom stack: {}'.format(zoom_state))
+
 
     @Slot()
     def zoom_back(self):
@@ -167,8 +130,7 @@ class ZoomStack(QObject):
             self.zoom_pos -= 1
             logger.debug(f'Zooming to position: {self.zoom_pos}')    
             logger.debug(f'Recalling from zoom stack: {self.stack[self.zoom_pos]}')    
-            # self.recall_view(self.stack[self.zoom_pos])
-            self.recall_view2(self.stack[self.zoom_pos])
+            self.recall_view(self.stack[self.zoom_pos])
 
         else:
             logger.debug('Beginning of zoom stack reached')
@@ -185,8 +147,7 @@ class ZoomStack(QObject):
         logger.debug('Zooming forward')
         if self.zoom_pos < (len(self.stack) - 1):
             self.zoom_pos += 1
-            # self.recall_view(self.stack[self.zoom_pos])
-            self.recall_view2(self.stack[self.zoom_pos])
+            self.recall_view(self.stack[self.zoom_pos])
         
         self.sigEnableBack.emit(self.zoom_pos > 0)
         self.sigEnableForward.emit(self.zoom_pos < (len(self.stack) - 1))
